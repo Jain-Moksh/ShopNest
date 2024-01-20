615267460e2e4b9ea7f7a66c88562b3fdf881635
Jain-Moksh <mokshjain008@gmail.com>
2024-01-20 20:10:49 +0530

    Updated names and done  setup of Routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+
+
+
 
 const App = () => {
-  return <div>Welcome to ShopNest E-Commerce Website</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} /> 
+        <Route path="/about" element={<About />} /> 
+        
+        
+      </Routes>
+    </Router>
+  )
 };
 
 export default App;
